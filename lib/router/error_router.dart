220fe83e8afb67eb48b2365da191e172e66d60fc
KoheiKanagu <kanagu@kingu.dev>
2022-09-23 21:43:17 +0900

    feat: error page (#20)

---
+++ lib/router/error_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:kingu_dev/pages/error/error_page.dart';
+
+part 'error_router.g.dart';
+
+@TypedGoRoute<ErrorPageRoute>(
+  path: ErrorPageRoute.path,
+)
+class ErrorPageRoute extends GoRouteData {
+  const ErrorPageRoute();
+
+  static const path = '/error';
+
+  @override
+  Widget build(BuildContext context) {
+    return const ErrorPage();
+  }
+}
--- lib/router/my_go_router.dart
+++ lib/router/my_go_router.dart
@@ -1,25 +1,36 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kingu_dev/firebase.dart';
 import 'package:kingu_dev/provider_logger.dart';
 
+import 'error_router.dart' as error_router;
 import 'home_router.dart' as home_router;
 
 final myGoRouter = Provider.autoDispose(
   (ref) => GoRouter(
     routes: [
       ...home_router.$appRoutes,
+      ...error_router.$appRoutes,
     ],
+    redirect: (_, state) {
+      return state.subloc != '/' ? '/' : null;
+    },
     errorBuilder: (context, state) {
       logger.e(
-        'router error',
-        state,
+        [
+          'router error',
+          'name: ${state.name}',
+          'fullpath: ${state.fullpath}',
+          'params: ${state.params}',
+          'location: ${state.location}',
+          'namqueryParametersAlle: ${state.queryParametersAll}',
+        ],
+        state.error,
         StackTrace.current,
       );
-      return const Text('error');
+      return const error_router.ErrorPageRoute().build(context);
     },
     observers: [
       FirebaseAnalyticsObserver(
