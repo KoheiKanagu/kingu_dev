7841952b08a51d77ed73503fbf017282b5eec510
KoheiKanagu <kanagu@kingu.dev>
2023-04-09 12:32:59 +0900

    fix

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -4,11 +4,7 @@ set -euxo pipefail
 rm -rf _flutter
 git clone https://github.com/flutter/flutter.git --depth 1 -b "stable" "_flutter"
 
-_flutter="$(pwd)/_flutter/bin"
-export PATH="$PATH:$_flutter"
-
 flutter --version
 
 flutter pub global activate melos
 flutter pub global activate grinder
-export PATH="$PATH":"$HOME/.pub-cache/bin"
