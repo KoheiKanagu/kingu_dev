38ae5c8fa3510ae843b62f286ceb2b3ebe27a24f
KoheiKanagu <kanagu@kingu.dev>
2023-04-09 15:46:41 +0900

    refactor: devcontainer (#191

---
+++ .devcontainer/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euxo pipefail
+
+if [ -d "$FLUTTER_HOME" ]; then
+    (cd "$FLUTTER_HOME" && git pull)
+else
+    git clone https://github.com/flutter/flutter.git --depth 1 -b "stable" "._flutter"
+fi
+
+flutter --version
+
+flutter pub global activate melos
+flutter pub global activate grinder
+
+flutter pub get
