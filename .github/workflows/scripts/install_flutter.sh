aba93a7398c0ffe7872e301a9e44c9204464e24a
KoheiKanagu <kanagu@kingu.dev>
2023-04-17 14:06:31 +0000

    fix

--- .github/workflows/scripts/install_flutter.sh
+++ .github/workflows/scripts/install_flutter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -d ".$GITHUB_WORKSPACE/_flutter" ]; then
+if [ -d "$GITHUB_WORKSPACE/_flutter" ]; then
     exit 0
 fi
 
 BRANCH=$1
-git clone https://github.com/flutter/flutter.git --depth 1 -b "$BRANCH" ".$GITHUB_WORKSPACE/_flutter"
-echo ".$GITHUB_WORKSPACE/_flutter/bin" >>"$GITHUB_PATH"
+git clone https://github.com/flutter/flutter.git --depth 1 -b "$BRANCH" "$GITHUB_WORKSPACE/_flutter"
+echo "$GITHUB_WORKSPACE/_flutter/bin" >>"$GITHUB_PATH"
