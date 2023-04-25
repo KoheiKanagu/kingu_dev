38557b268ec762a593005a157c69781b7ad1aed8
KoheiKanagu <kanagu@kingu.dev>
2023-04-25 10:26:15 +0900

    build: add gh extension (#222)

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Install Flutter
 if [ -d "$FLUTTER_HOME" ]; then
     (cd "$FLUTTER_HOME" && git pull)
 else
@@ -13,3 +14,7 @@ flutter pub global activate melos
 flutter pub global activate grinder
 
 flutter pub get
+
+# gh extension
+
+gh extension install seachicken/gh-poi
