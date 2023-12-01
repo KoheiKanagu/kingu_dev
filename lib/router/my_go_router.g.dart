a8a29545002dc6f2098751bee44f5de2e4fcaaa0
KoheiKanagu <kanagu@kingu.dev>
2023-12-02 00:18:16 +0900

    fix

--- lib/router/my_go_router.g.dart
+++ lib/router/my_go_router.g.dart
@@ -3,130 +3,22 @@
 part of 'my_go_router.dart';
 
 // **************************************************************************
-// GoRouterGenerator
+// RiverpodGenerator
 // **************************************************************************
 
-List<RouteBase> get $appRoutes => [
-      $rootPageRoute,
-    ];
+String _$myGoRouterHash() => r'8da751cc6699ce11430a90d22d47c1fc079bb347';
 
-RouteBase get $rootPageRoute => ShellRouteData.$route(
-      navigatorKey: RootPageRoute.$navigatorKey,
-      factory: $RootPageRouteExtension._fromState,
-      routes: [
-        GoRouteData.$route(
-          path: '/error',
-          factory: $ErrorPageRouteExtension._fromState,
-        ),
-        GoRouteData.$route(
-          path: '/settings',
-          factory: $SettingsPageRouteExtension._fromState,
-        ),
-        GoRouteData.$route(
-          path: '/profile',
-          factory: $ProfilePageRouteExtension._fromState,
-        ),
-        GoRouteData.$route(
-          path: '/games',
-          factory: $GamePageRouteExtension._fromState,
-          routes: [
-            GoRouteData.$route(
-              path: 'pong',
-              factory: $PongPageRouteExtension._fromState,
-            ),
-          ],
-        ),
-      ],
-    );
+/// See also [myGoRouter].
+@ProviderFor(myGoRouter)
+final myGoRouterProvider = AutoDisposeProvider<Raw<GoRouter>>.internal(
+  myGoRouter,
+  name: r'myGoRouterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myGoRouterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
 
-extension $RootPageRouteExtension on RootPageRoute {
-  static RootPageRoute _fromState(GoRouterState state) => const RootPageRoute();
-}
-
-extension $ErrorPageRouteExtension on ErrorPageRoute {
-  static ErrorPageRoute _fromState(GoRouterState state) =>
-      const ErrorPageRoute();
-
-  String get location => GoRouteData.$location(
-        '/error',
-      );
-
-  void go(BuildContext context) => context.go(location);
-
-  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
-
-  void pushReplacement(BuildContext context) =>
-      context.pushReplacement(location);
-
-  void replace(BuildContext context) => context.replace(location);
-}
-
-extension $SettingsPageRouteExtension on SettingsPageRoute {
-  static SettingsPageRoute _fromState(GoRouterState state) =>
-      const SettingsPageRoute();
-
-  String get location => GoRouteData.$location(
-        '/settings',
-      );
-
-  void go(BuildContext context) => context.go(location);
-
-  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
-
-  void pushReplacement(BuildContext context) =>
-      context.pushReplacement(location);
-
-  void replace(BuildContext context) => context.replace(location);
-}
-
-extension $ProfilePageRouteExtension on ProfilePageRoute {
-  static ProfilePageRoute _fromState(GoRouterState state) =>
-      const ProfilePageRoute();
-
-  String get location => GoRouteData.$location(
-        '/profile',
-      );
-
-  void go(BuildContext context) => context.go(location);
-
-  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
-
-  void pushReplacement(BuildContext context) =>
-      context.pushReplacement(location);
-
-  void replace(BuildContext context) => context.replace(location);
-}
-
-extension $GamePageRouteExtension on GamePageRoute {
-  static GamePageRoute _fromState(GoRouterState state) => const GamePageRoute();
-
-  String get location => GoRouteData.$location(
-        '/games',
-      );
-
-  void go(BuildContext context) => context.go(location);
-
-  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
-
-  void pushReplacement(BuildContext context) =>
-      context.pushReplacement(location);
-
-  void replace(BuildContext context) => context.replace(location);
-}
-
-extension $PongPageRouteExtension on PongPageRoute {
-  static PongPageRoute _fromState(GoRouterState state) => const PongPageRoute();
-
-  String get location => GoRouteData.$location(
-        '/games/pong',
-      );
-
-  void go(BuildContext context) => context.go(location);
-
-  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
-
-  void pushReplacement(BuildContext context) =>
-      context.pushReplacement(location);
-
-  void replace(BuildContext context) => context.replace(location);
-}
+typedef MyGoRouterRef = AutoDisposeProviderRef<Raw<GoRouter>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
