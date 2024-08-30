c1061eb5d7b66044e3d9bc23dbbcea806215ce91
KoheiKanagu <kanagu@kingu.dev>
2024-08-30 10:08:33 +0900

    fix

--- convert.sh
+++ convert.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-find md -name "*.md" | while IFS= read -r file; do
-    path=${file#md/}
+rm -rf public
+mkdir -p public
 
-    outFile="public/${path%.md}.html"
-    mkdir -p "$(dirname "$outFile")"
+for file in md/*.md; do
+    outFile="public/$(basename "$file" .md).html"
 
     pandoc --standalone \
         --from markdown \
@@ -17,5 +17,24 @@ find md -name "*.md" | while IFS= read -r file; do
         --shift-heading-level-by=-1 \
         --fail-if-warnings \
         --metadata title="kingu.dev" \
+        "$file" >"$outFile"
+done
+
+for file in md/apps/**/*.md; do
+    # md/apps/hoge/index.md → public/apps/hoge/index.html
+    outFile="$(dirname "$file" | sed 's/md/public/')/index.html"
+
+    mkdir -p "$(dirname "$outFile")"
+
+    pandoc --standalone \
+        --from markdown \
+        --to html \
+        --template .pandoc/templates/uikit_apps.html \
+        --css ../../../.pandoc/templates/styles.css \
+        --toc \
+        --toc-depth=3 \
+        --shift-heading-level-by=-1 \
+        --fail-if-warnings \
+        --metadata title="$(basename "$(dirname "$file")")" \
         "$file" >"$outFile"
 done
---
+++ md/apps/obento/index.md
@@ -0,0 +1,19 @@
+# #お弁当記録 / #obento
+
+<img id="app_icon_big"
+    src="../../../images/apps/obento.webp"
+    alt="#お弁当記録 / #obento"
+  />
+
+<a href="https://apps.apple.com/us/app/obento/id6499041461?itsct=apps_box_link&itscg=30200">
+<img id="app_store_badge_big"
+    src="../../../images/app_store.svg"
+    alt="App Store"
+  />
+</a>
+<a href="https://play.google.com/store/apps/details?id=dev.kingu.obento">
+<img id="google_play_badge_big"
+    src="../../../images/google_play.webp"
+    alt="Google Play"
+  />
+</a>
